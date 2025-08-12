5b830ae9ff06dc012744e18a29576d13140dd38a
khanhchi207 <treesenef@gmail.com>
2025-08-12 21:17:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,29 @@
-"use strict";
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+const questionPrompt = document.querySelector(".question-prompt"); // Thêm đoạn này
 
-const titleElement = document.querySelector(".title");
-const buttonsContainer = document.querySelector(".buttons");
-const yesButton = document.querySelector(".btn--yes");
-const noButton = document.querySelector(".btn--no");
-const catImg = document.querySelector(".cat-img");
-
-const MAX_IMAGES = 5;
-
-let play = true;
-let noCount = 0;
-
-yesButton.addEventListener("click", handleYesClick);
-
-noButton.addEventListener("click", function () {
-  if (play) {
-    noCount++;
-    const imageIndex = Math.min(noCount, MAX_IMAGES);
-    changeImage(imageIndex);
-    resizeYesButton();
-    updateNoButtonText();
-    if (noCount === MAX_IMAGES) {
-      play = false;
-    }
-  }
-});
-
-function handleYesClick() {
-  titleElement.innerHTML = "Em iu anh bé ,Em hứa hongg làm anh bé buồn nữa đouuu :3";
-  buttonsContainer.classList.add("hidden");
-  changeImage("yes");
-}
 yesBtn.addEventListener("click", () => {
   question.innerHTML = tôiy 🥺<br>dỗiiii x1 tỷ";
   gif.src =
     "https://media.giphy.com/media/2weSkZg9hvQW5Zv2fk/giphy.gif";
 
-function resizeYesButton() {
-  const computedStyle = window.getComputedStyle(yesButton);
-  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
-  const newFontSize = fontSize * 1.6;
+  // Ẩn cả hai nút Yes và No
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+  questionPrompt.style.display = "none"; // Ẩn dòng chữ khi người dùng nhấn vào nút Yes
+});
 
-  yesButton.style.fontSize = `${newFontSize}px`;
-}
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
 
-function generateMessage(noCount) {
-  const messages = [
-    "Không Bao Giờ",
-    "Em bicc lỗi rồi ạa",
-    "Mong anh bé tha lỗi choo emmmm :((",
-    "Emmm saii rồi , emm đáng trách ạ",
-    "Anh bé đừng giận emm nữa nhoo",
-    "em iu anh bé nhắm nhunnn đóoooo",
-  ];
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
 
-  const messageIndex = Math.min(noCount, messages.length - 1);
-  return messages[messageIndex];
-}
-
-function changeImage(image) {
-  gif.src = https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExaDg2N2JxMnc0bmVhcHRlcmx0anoydW9jaTF5Z3FvNXJvOTJ0YzltdyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/KztT2c4u8mYYUiMKdJ/giphy.gif;
-}
-
-function updateNoButtonText() {
-  noButton.innerHTML = generateMessage(noCount);
-}
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
